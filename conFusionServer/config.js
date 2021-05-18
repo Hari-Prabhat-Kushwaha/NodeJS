7fa8afcfc52f5a90f52d1db6d2aba74d50443bfb
Hari Prabhat Kushwaha <hprabhatkushwaha@gmail.com>
2021-05-18 21:49:45 +0530

    123

---
+++ conFusionServer/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'secretKey': '12345-67890-09876-54321',
+	'mongoUrl': 'mongodb://localhost:27017/conFusion',
+	'facebook': {
+		clientId: '2386840618285195',
+		clientSecret: '81c3a3697f88bdf81ee101c20b7383bb'
+	}
+
+}
